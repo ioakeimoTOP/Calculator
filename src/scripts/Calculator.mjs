34496c38a03bbc2c66a87ba9cde879ce8edcdb36
ioakeimo <ioakeimo.dev@gmail.com>
2023-04-30 22:41:17 +0300

    Modify Calculator Class according to Observable changes

--- src/scripts/Calculator.mjs
+++ src/scripts/Calculator.mjs
@@ -1,11 +1,32 @@
 import Observable from './Observable.mjs';
+import { operate } from './mathOperations.mjs';
 
 export default class Calculator extends Observable {
-  constructor(initialState, ...observerFunctions) {
-    super(initialState, ...observerFunctions);
+  #state = {
+    current: '0',
+    history: [],
+  };
+
+  constructor(...observerFunctions) {
+    super(...observerFunctions);
   }
 
-  _mutate(newState) {
-    this._state = newState;
+  _getState() {
+    return this.#state.current;
+  }
+
+  inputNumber(number) {
+    if (number === '.') {
+      if (this.#state.current.includes('.')) {
+        return; // If there is already a . in the number an other should not be added
+      }
+      this.#state.current += number;
+      console.log('. added');
+    } else if (this.#state.current === '0') {
+      this.#state.current = number;
+    } else {
+      this.#state.current += number;
+    }
+    this._dispatchStateChange();
   }
 }
