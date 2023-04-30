18c6bb664124739af5563eee192af069505068d6
ioakeimo <ioakeimo.dev@gmail.com>
2023-04-30 20:56:10 +0300

    Simplify Observable to resemble an interface
    Some functionality in the Observable feels out of place. As the ideal
    implementation would be an Interface, a decision has been made to leave
    the entirety of state implementation to the Concrete SubClasses.

--- src/scripts/Observable.mjs
+++ src/scripts/Observable.mjs
@@ -1,11 +1,10 @@
 /**
  * Abstract Class Observable
+ * Provide an interface for subscribing observers and notifying them.
  *
- * Provide a state that can be subscribed to by callback observer functions.
- * On state change, notifies observers with the new state.
+ * Concrete Subclasses MUST implement _getState() and all state logic.
  */
 export default class Observable {
-  _state;
   #observers;
 
   #validateIsFunction(arg) {
@@ -18,14 +17,12 @@ export default class Observable {
     }
   }
 
-  constructor(initialState, ...observerFunctions) {
+  constructor(...observerFunctions) {
     if (this.constructor === Observable) {
       throw new Error(
         `${this.constructor.name} is an Abstract Class and should not be instantiated.`
       );
     }
-
-    this._state = initialState;
     observerFunctions?.forEach(this.#validateIsFunction);
     this.#observers = new Set(observerFunctions);
   }
@@ -39,28 +36,15 @@ export default class Observable {
     this.#observers.delete(observerFunction);
   }
 
-  /**
-   * Overwrite this accessor if only part of a state is needed.
-   * For example, if the state is a history stack, you may want to define the accessor as _state.at(-1)
-   */
   _getState() {
-    return this._state;
-  }
-
-  #dispatchStateChange() {
-    this.#observers.forEach((observer) => {
-      observer(this._getState());
-    });
-  }
-
-  _mutate(argsArray) {
     throw new Error(
-      'This method must be overwritten by the Subclass to define a state change behavior.'
+      'This method must be overwritten by the Subclass to define a state to be returned.'
     );
   }
 
-  setState(...mutateArgs) {
-    this._mutate(mutateArgs);
-    this.#dispatchStateChange();
+  _dispatchStateChange() {
+    this.#observers.forEach((observer) => {
+      observer(this._getState());
+    });
   }
 }
