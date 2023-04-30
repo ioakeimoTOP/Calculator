370b6749e2e00c07a3bf79f8083f76ae1acd607e
ioakeimo <ioakeimo.dev@gmail.com>
2023-04-30 22:42:49 +0300

    Modify index html/js to use the new Calculator

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,12 +4,17 @@ const screen = document.querySelector('.calculator__screen');
 function updateScreen(newScreenState) {
   screen.textContent = newScreenState;
 }
-const screenState = new Calculator(0, updateScreen);
+const calculator = new Calculator();
+calculator.subscribe(updateScreen);
 
 const numpad = document.querySelector('.calculator__numpad');
 numpad.addEventListener('click', (event) => {
   const target = event.target;
   if (target.classList.contains('calculator__item')) {
-    screenState.setState(target.textContent);
+    if (target?.dataset.operation === '.') {
+      calculator.inputDot();
+    } else {
+      calculator.inputNumber(target.textContent);
+    }
   }
 });
