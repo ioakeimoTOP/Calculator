b3c41350cc8c2a5ae5a8c10b542bae864b6ed207
ioakeimo <ioakeimo.dev@gmail.com>
2023-05-01 19:04:41 +0300

    Modify the math operations module to work with and return strings, rename it accordingly

--- src/scripts/stringMathOperations.mjs
+++ src/scripts/stringMathOperations.mjs
@@ -6,19 +6,19 @@ const operatorMap = {
 };
 
 function _add(num1, num2) {
-  return num1 + num2;
+  return (Number(num1) + Number(num2)).toString();
 }
 
 function _subtract(num1, num2) {
-  return num1 - num2;
+  return (Number(num1) - Number(num2)).toString();
 }
 
 function _multiply(num1, num2) {
-  return num1 * num2;
+  return (Number(num1) * Number(num2)).toString();
 }
 
 function _divide(num1, num2) {
-  return num1 / num2;
+  return (Number(num1) / Number(num2)).toString();
 }
 
 function operate(operator, operand1, operand2) {
